dd33af7c8bad55fb12dd265d57819fee97103244
BlueSkyFish <blueskyfish@blueskyfish.de>
2024-03-16 14:14:42 +0100

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,8 @@ class CalendarApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: const StartPage(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
 }
-
